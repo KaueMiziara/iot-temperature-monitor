255bb5f3a8e605645284e0585cbe1d47b4e2a067
Kaue Miziara <kauemiziara@gmail.com>
2023-10-07 20:57:51 -0300

    Add one wire dependency && Set up peripherals and GPIO

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+use esp_idf_hal::{gpio::*, peripherals::Peripherals};
 use esp_idf_sys as _;
 use log::*;
+use onewire::OneWire;
 
 fn main() {
     esp_idf_sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
+
+    let peripherals = Peripherals::take().unwrap();
+    let mut one = PinDriver::input_output(peripherals.pins.gpio18).unwrap();
+    let mut _wire = OneWire::new(&mut one, false);
 
     info!("Hello, world!");
 }
